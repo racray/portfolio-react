9c84a8daf66d7adeb3cbead81121dabe93f6d2e2
racray <rayedkhan007@gmail.com>
2022-03-09 10:44:12 +0530

    vue quasar

--- src/data.jsx
+++ src/data.jsx
@@ -1,104 +1,5 @@
-export const webPortfolio = [
-  {
-    id: 1,
-    title: "Travel Map",
-    img: "assets/map-snip.png",
-    web: "https://github.com/racray/frontend",
-  },
 
-  {
-    id: 2,
-    title: "E-commerce app",
-    img: "assets/ecommerce-snip.png",
-    web: "https://kind-keller-3d8edb.netlify.app/",
-  },
-  {
-    id: 3,
-    title: "Movies App",
-    img: "assets/movies-snip.png",
-    web: "https://ecstatic-elion-ae06f7.netlify.app/",
-  },
-];
 
-export const designPortfolio = [
-  {
-    id: 1,
-    title: "React Web design",
-    img: "https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/133707519/original/a16075311dd3d9d8c27c3c6f3ba046ed7704b68c/create-react-redux-gatsby-graphql-website.png ",
-    web: "https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/133707519/original/a16075311dd3d9d8c27c3c6f3ba046ed7704b68c/create-react-redux-gatsby-graphql-website.png ",
-  },
-  {
-    id: 2,
-    title: "React Web design",
-    img: "https://www.creative-tim.com/blog/content/images/wordpress/2019/04/opt_argon_react_thumbnail.jpg",
-    web: "https://www.creative-tim.com/blog/content/images/wordpress/2019/04/opt_argon_react_thumbnail.jpg",
-  },
-  {
-    id: 3,
-    title: "React Web design",
-    img: "https://madewithreact.com/assets/img/react-material-bootstrap-dashboard-pro.jpg?v=1f2ec887e3",
-    web: "https://madewithreact.com/assets/img/react-material-bootstrap-dashboard-pro.jpg?v=1f2ec887e3",
-  },
-  {
-    id: 4,
-    title: "React Web Portfolio",
-    img: "assets/tempsnip.png",
-    web: "https://trusting-mcnulty-a4bde2.netlify.app/",
-  },
-];
-
-export const resumePortfolio = [
-  {
-    id: 1,
-    title: "Resume of Rayed",
-    img: "assets/resume.png",
-    web: "https://docs.google.com/document/d/1pxQ13Lx8ecTi-Ob9j6DIQVCANtTUgtZMaOF6dtqwjWo/edit",
-  },
-];
-
-export const backendPortfolio = [
-  {
-    id: 1,
-    title: "Travel Map",
-    img: "assets/map-snip.png",
-    web: "https://github.com/racray/MERN-map-backend",
-  },
-
-  {
-    id: 2,
-    title: "E-commerce app",
-    img: "assets/ecommerce-snip.png",
-    web: "https://github.com/racray/Ecommerceapi",
-  },
-  {
-    id: 3,
-    title: "Movies App",
-    img: "assets/movies-snip.png",
-    web: "https://github.com/racray/b28wd-nodejs",
-  },
-];
-
-export const frontendPortfolio = [
-  {
-    id: 1,
-    title: "Travel Map",
-    img: "assets/map-snip.png",
-    web: "https://github.com/racray/frontend",
-  },
-
-  {
-    id: 2,
-    title: "E-commerce app",
-    img: "assets/ecommerce-snip.png",
-    web: "https://github.com/racray/Ecommerceclient",
-  },
-  {
-    id: 3,
-    title: "Movies App",
-    img: "assets/movies-snip.png",
-    web: "https://github.com/racray/racray-react-test1",
-  },
-];
 
 export const FrontskillsData = [
   {
@@ -121,6 +22,17 @@ export const FrontskillsData = [
     title: "React",
     progBar: 80,
   },
+  {
+    id:5,
+    title:"Vue",
+    progBar: 70,
+  },
+  {
+    id:6,
+    title:"Quasar",
+    progBar: 65,
+  },
+
 ];
 
 export const BackskillsData = [
